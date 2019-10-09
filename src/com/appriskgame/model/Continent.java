45d2b519121d495ee4b3c27abe3f51a5d82da5c8
shruthikvenkat <shruthi.kv20@gmail.com>
2019-10-09 00:31:24 -0400

    JavaDoc for Classes Country and Continent

--- src/com/appriskgame/model/Continent.java
+++ src/com/appriskgame/model/Continent.java
@@ -2,32 +2,73 @@ package com.appriskgame.model;
 
 import java.util.ArrayList;
 
-public class Continent {
+/**
+ * The Continent class is to have all the details regarding Continent
+ *
+ * @author Shruthi
+ *
+ */
 
+public class Continent {
+	/** The name of Continent */
 	private String continentName;
+
+	/** Control value for each continent */
 	private int continentControlValue;
+
+	/** List of all countries a continents must have */
 	private ArrayList<Country> listOfCountries = new ArrayList<Country>();
 
+	/**
+	 * Get the continent name.
+	 *
+	 * @return continent's Name
+	 */
 	public String getContinentName() {
 		return continentName;
 	}
 
+	/**
+	 * Set the continent name.
+	 *
+	 * @param name To set the name of the continent
+	 */
 	public void setContinentName(String continentName) {
 		this.continentName = continentName;
 	}
 
+	/**
+	 * To get the Control value for each continent
+	 *
+	 * @return continentControlValue
+	 */
 	public int getContinentControlValue() {
 		return continentControlValue;
 	}
 
+	/**
+	 * To set the Control value for each continent
+	 *
+	 * @param continentControlValue
+	 */
 	public void setContinentControlValue(int continentControlValue) {
 		this.continentControlValue = continentControlValue;
 	}
 
+	/**
+	 * Get the list of countries in the particular continent.
+	 *
+	 * @return listOfContries
+	 */
 	public ArrayList<Country> getListOfCountries() {
 		return listOfCountries;
 	}
 
+	/**
+	 * Set the list of countries in the particular continent.
+	 *
+	 * @param listOfContries
+	 */
 	public void setListOfCountries(ArrayList<Country> listOfCountries) {
 		this.listOfCountries = listOfCountries;
 	}
--- src/com/appriskgame/model/Country.java
+++ src/com/appriskgame/model/Country.java
@@ -3,80 +3,140 @@ package com.appriskgame.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Country class is for all the details about a country
+ *
+ * @author Shruthi
+ */
 public class Country {
-
+	/** Name of country. */
 	private String countryName;
+
+	/** The Continent name of the respective country */
 	private String continentName;
+
+	/** The neighboring Countries with respect to countries Continent */
 	private ArrayList<String> neighbourCountries=new ArrayList<String>();;
 	private List<Country> neighbourCountriesToAdd = new ArrayList<Country>();
+
+	/** Part of continent */
 	private Continent partOfContinent;
 	private int armiesCount;
 	private String player;
 
+	/**
+	 * To get the CountryName
+	 *
+	 * @return countryName
+	 */
 	public String getCountryName() {
 		return countryName;
 	}
 
+	/**
+	 * To set the CountryName
+	 *
+	 * @param countryName
+	 */
 	public void setCountryName(String countryName) {
 		this.countryName = countryName;
 	}
 
 
-
+	/**
+	 * To get the Continent name of the respective country
+	 *
+	 * @return continentName
+	 */
 	public String getContinentName() {
 		return continentName;
 	}
 
+	/**
+	 * To set the Continent name of the respective country
+	 *
+	 * @param continentName
+	 */
 	public void setContinentName(String continentName) {
 		this.continentName = continentName;
 	}
 
+	/**
+	 * To get the Neighbor Countries To Add
+	 *
+	 * @return neighbourCountriesToAdd
+	 */
 	public List<Country> getNeighbourCountriesToAdd() {
 		return neighbourCountriesToAdd;
 	}
 
+	/**
+	 * To set the Neighbor Countries
+	 *
+	 * @param neighbourCountriesToAdd
+	 */
 	public void setNeighbourCountriesToAdd(List<Country> neighbourCountriesToAdd) {
 		this.neighbourCountriesToAdd = neighbourCountriesToAdd;
 	}
 
+	/**
+	 * To get the Number of armies for each country
+	 *
+	 * @return armiesCount
+	 */
 	public int getNoOfArmies() {
 		return armiesCount;
 	}
 
+	/**
+	 * To set the Number of countries for each country
+	 *
+	 * @param noOfArmies
+	 */
 	public void setNoOfArmies(int noOfArmies) {
 		this.armiesCount = noOfArmies;
 	}
 
 	/**
 	 * Get the list of adjacent countries.
-	 * 
-	 * @return adjacent Countries
+	 *
+	 * @return neighbourCountries
 	 */
 	public ArrayList<String> getNeighbourCountries() {
 		return neighbourCountries;
 	}
 
+	/**
+	 * Set the list of adjacent countries.
+	 *
+	 * @param neighbourCountries
+	 */
 	public void setNeighbourCountries(ArrayList<String> neighbourCountries) {
 		this.neighbourCountries = neighbourCountries;
 	}
 
 	/**
-	 * Get the continent name to which country belongs to.
-	 * 
-	 * @return Name of the continent
+	 * Get the continent object which country belongs to.
+	 *
+	 * @return partOfContinent
 	 */
 	public Continent getPartOfContinent() {
 		return partOfContinent;
 	}
 
+	/**
+	 * Set the continent object which country belongs to.
+	 *
+	 * @param partOfContinent
+	 */
 	public void setPartOfContinent(Continent partOfContinent) {
 		this.partOfContinent = partOfContinent;
 	}
 
 	/**
 	 * Get the player name to whom country belong to.
-	 * 
-	 * @return Name of the player
+	 *
+	 * @return player
 	 */
 	public String getPlayer() {
 		return player;
@@ -84,8 +144,8 @@ public class Country {
 
 	/**
 	 * Set the player name to whom country belong to.
-	 * 
-	 * @param player name To set the name of the player
+	 *
+	 * @param player
 	 */
 	public void setPlayer(String player) {
 		this.player = player;
