ae4bf0259a13f9d453bb7618164c8fbe55d5ae68
SahanaAnantha <sahana.anantha93@gmail.com>
2019-10-16 02:48:50 -0400

    Map Validation TestCase

--- src/com/appriskgame/test/ReinforcementPhaseTest.java
+++ src/com/appriskgame/test/ReinforcementPhaseTest.java
@@ -1,8 +1,10 @@
 package com.appriskgame.test;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+
 import org.junit.Before;
+import org.junit.Test;
+
 import com.appriskgame.model.Continent;
 import com.appriskgame.model.Country;
 import com.appriskgame.model.GamePlayer;
@@ -17,7 +19,9 @@ import com.appriskgame.services.ReinforcementPhase;
 public class ReinforcementPhaseTest {
 
 	GamePlayer player1, player2, player3;
-	Country country1, country2, country3, country4;
+
+	Country country1, country2, country3, country4, country5, country6, country7, country8, country9, country10;
+
 	Continent continent, continent1;
 
 	/**
@@ -26,30 +30,51 @@ public class ReinforcementPhaseTest {
 	@Before
 	public void initializeReinforcementPhaseTest() {
 		continent = new Continent();
+
 		player1 = new GamePlayer();
 		player2 = new GamePlayer();
+
 		continent.setContinentName("Asia");
 		continent.setContinentControlValue(4);
+
 		continent1.setContinentName("Africa");
 		continent.setContinentControlValue(2);
+
 		country1 = new Country();
 		country1.setCountryName("India");
 		player1.getPlayerCountries().add(country1);
 		player2.getPlayerCountries().add(country1);
 		continent.getListOfCountries().add(country1);
+
 		country2 = new Country();
 		country2.setCountryName("China");
 		player1.getPlayerCountries().add(country2);
 		player2.getPlayerCountries().add(country2);
 		continent.getListOfCountries().add(country2);
+
 		country3 = new Country();
 		country3.setCountryName("Srilanka");
 		player1.getPlayerCountries().add(country3);
 		player2.getPlayerCountries().add(country3);
+//		continent.getListOfCountries().add(country3);
+
 		country4 = new Country();
 		country4.setCountryName("Sounth Africa");
 		player1.getPlayerCountries().add(country4);
 		player2.getPlayerCountries().add(country4);
+		country5 = new Country();
+		country6 = new Country();
+		country7 = new Country();
+		country8 = new Country();
+		country9 = new Country();
+		country10 = new Country();
+		player2.getPlayerCountries().add(country5);
+		player2.getPlayerCountries().add(country6);
+		player2.getPlayerCountries().add(country7);
+		player2.getPlayerCountries().add(country8);
+		player2.getPlayerCountries().add(country9);
+		player2.getPlayerCountries().add(country10);
+//		continent.getListOfCountries().add(country4);
 	}
 
 	/**
@@ -68,6 +93,6 @@ public class ReinforcementPhaseTest {
 	public void testassignReinforcedArmies2() {
 		int expected = 4;
 		continent.getListOfCountries().add(country3);
-		assertEquals(expected, ReinforcementPhase.assignReinforcedArmies(player1, continent));
+		assertEquals(expected, ReinforcementPhase.assignReinforcedArmies(player2, continent));
 	}
 }
