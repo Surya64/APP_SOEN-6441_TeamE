67d44ee9ff5d95262f14b66950387e26ca398f33
SahanaAnantha <sahana.anantha93@gmail.com>
2019-10-16 01:33:27 -0400

    Junit Test Cases update

--- src/com/appriskgame/test/JUnitTestSuite.java
+++ src/com/appriskgame/test/JUnitTestSuite.java
@@ -1,5 +1,18 @@
 package com.appriskgame.test;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MapOperationTest.class, MapValidationTest.class, ReinforcementPhaseTest.class, StartupPhaseTest.class })
+
+/**
+ * A TestSuite class for testing all the test cases
+ * 
+ * @author Sahana
+ *
+ */
 public class JUnitTestSuite {
 
 }
