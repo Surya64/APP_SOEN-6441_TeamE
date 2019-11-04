482d2c143ba323db91345bb1de414d1a66ca0596
dolly771997 <modhadolly@gmail.com>
2019-11-04 17:07:43 -0500

    Player.java updated.

--- src/com/appriskgame/controller/Player.java
+++ src/com/appriskgame/controller/Player.java
@@ -1824,4 +1824,9 @@ public class Player {
 		playerObject.setPlayerCountries(updatedCountryList);
 		return playerObject;
 	}
+
+	public void attackPhaseControl(GameMap gameMap, Object attackercountry, Country toCountry) {
+		// TODO Auto-generated method stub
+		
+	}
 }
