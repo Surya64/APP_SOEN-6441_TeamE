591db1fa5b3ef90af3654f4875a06e95bec819f0
surya64 <suryaprakash.govindaraju@gmail.com>
2019-11-19 18:20:40 -0500

    Player Strategy creation

---
+++ src/com/appriskgame/strategy/Aggressive.java
@@ -0,0 +1,34 @@
+package com.appriskgame.strategy;
+
+import com.appriskgame.model.Country;
+import com.appriskgame.model.GameMap;
+import com.appriskgame.model.GamePlayer;
+
+public class Aggressive implements PlayerStrategy {
+
+	@Override
+	public void placeArmies(GameMap gameMap, GamePlayer player, Country country) {
+		
+		
+	}
+
+	@Override
+	public void reinforcementPhase(GamePlayer player, GameMap gameMap, Country country, int reinforceArmyCount) {
+		
+		
+	}
+
+	@Override
+	public void attackPhase(GameMap gameMap, GamePlayer player, Country attackerCountry, Country defenderCountry) {
+		
+		
+	}
+
+	@Override
+	public void fortificationPhase(GameMap gameMap, GamePlayer player, Country fromCountry, Country toCountry,
+			int armiesCount) {
+		
+		
+	}
+
+}
---
+++ src/com/appriskgame/strategy/Benevolent.java
@@ -0,0 +1,34 @@
+package com.appriskgame.strategy;
+
+import com.appriskgame.model.Country;
+import com.appriskgame.model.GameMap;
+import com.appriskgame.model.GamePlayer;
+
+public class Benevolent implements PlayerStrategy {
+
+	@Override
+	public void placeArmies(GameMap gameMap, GamePlayer player, Country country) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reinforcementPhase(GamePlayer player, GameMap gameMap, Country country, int reinforceArmyCount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attackPhase(GameMap gameMap, GamePlayer player, Country attackerCountry, Country defenderCountry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fortificationPhase(GameMap gameMap, GamePlayer player, Country fromCountry, Country toCountry,
+			int armiesCount) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
