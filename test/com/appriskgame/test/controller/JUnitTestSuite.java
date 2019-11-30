17321de5b87bff4078abcf6543886b4c53ab690b
surya64 <suryaprakash.govindaraju@gmail.com>
2019-11-30 00:01:21 -0500

    Junit structure changes and update

--- test/com/appriskgame/test/controller/JUnitTestSuite.java
+++ test/com/appriskgame/test/controller/JUnitTestSuite.java
@@ -4,9 +4,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.appriskgame.strategy.Benevolent;
+import com.appriskgame.test.strategy.AggressiveTest;
+import com.appriskgame.test.strategy.BenevolentTest;
+import com.appriskgame.test.strategy.CheaterTest;
+import com.appriskgame.test.strategy.RandomPlayerTest;
+
 @RunWith(Suite.class)
 @SuiteClasses({ TournamentTest.class, MapOperationTest.class, MapValidationTest.class, PlayerTest.class,
-		CardControllerTest.class })
+		CardControllerTest.class, AggressiveTest.class, BenevolentTest.class, CheaterTest.class,
+		RandomPlayerTest.class })
 
 /**
  * A TestSuite class for testing all the test cases
